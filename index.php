e6924c0661c71a0ac0228419f3044bf97742794e
Ryan Maskell <ryan@savorweb.com>
2014-03-13 12:20:48 -0700

    version0

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<!--[if lt IE 9 ]> <html lang="en-GB" class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9 ]> <html lang="en-GB" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-GB" class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="initial-scale=1">
+
+		<title>Semantic.gs + Sassaparilla</title>
+		<link href="assets/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>		
+		<!-- <link rel="shortcut icon" type="image/ico" href="favicon.ico" /> -->
+		<!-- TODO: Download a custom build of modernizer to suit the project -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
+	</head>
+
+	<body>
+
+		
+
+			<!-- Header -->
+	<header class="top">
+		<h1><a href="http://semantic.gs/">The Semantic Grid System</a> + <a href="http://sass.fffunction.co/">Sassaparilla</a></h1>
+		<p>A responsive SCSS framework combined by Ryan Maskell</p>
+	</header>
+
+	<!-- Main Body -->
+	<article class="maincolumn">
+		<h2>Main</h2>
+	</article>
+
+	<!-- Sidebar -->
+	<section class="sidebar">
+		<h2>Sidebar</h2>
+	</section>
+			
+
+		<!-- TODO: Select version of jquery and link to CDN -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="assets/js/script.min.js"></script>
+
+	</body>
+
+</html>
