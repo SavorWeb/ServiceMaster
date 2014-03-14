202cc9ed079368f133a2b35cb42c8fda52f6a8a6
Ryan Maskell <ryan@savorweb.com>
2014-03-13 17:12:06 -0700

    Mobile Navigation

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 		<meta name="viewport" content="initial-scale=1">
 
 		<title>Semantic.gs + Sassaparilla</title>
-		<link href="assets/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>		
+		<link rel="stylesheet" href="assets/css/add-ons/responsive-nav.min.css">
+		<link href="assets/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>
 		<!-- <link rel="shortcut icon" type="image/ico" href="favicon.ico" /> -->
 		<!-- TODO: Download a custom build of modernizer to suit the project -->
 		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
@@ -16,29 +17,41 @@
 
 	<body>
 
-		
+	<header>
+		<object class="logo" type="image/svg+xml" data="assets/img/logo.svg" alt="ServiceMaster Logo"></object>
 
-			<!-- Header -->
-	<header class="top">
-		<h1><a href="http://semantic.gs/">The Semantic Grid System</a> + <a href="http://sass.fffunction.co/">Sassaparilla</a></h1>
-		<p>A responsive SCSS framework combined by Ryan Maskell</p>
+		<button href="#" type="button" role="button" class="nav-toggle closed lines-button x2"><span class="lines"></span></button>
+		
+		<nav class="nav-collapse">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">About us</a></li>
+				<li><a href="">Services</a></li>
+				<li><a href="">Claims</a></li>
+				<li><a href="">FAQs</a></li>
+				<li><a href="">Contact us</a></li>
+			</ul>
+		</nav>
+		
 	</header>
 
-	<!-- Main Body -->
-	<article class="maincolumn">
-		<h2>Main</h2>
-	</article>
-
-	<!-- Sidebar -->
-	<section class="sidebar">
-		<h2>Sidebar</h2>
-	</section>
 			
 
 		<!-- TODO: Select version of jquery and link to CDN -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="assets/js/script.min.js"></script>
+		<script src="assets/js/responsive-nav.js"></script>
+		<script>
+	      // var navigation = responsiveNav(".nav-collapse");
 
+	      var navigation = responsiveNav(".nav-collapse", {
+	        customToggle: ".nav-toggle"
+	      });
+
+			$(".nav-toggle").click(function(){
+				$(this).toggleClass("close");
+			});
+		</script>
 	</body>
 
 </html>
