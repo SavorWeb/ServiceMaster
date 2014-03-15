7a495cde6e449d4b63fecc31feefa4b3451d9b44
Ryan Maskell <ryan@savorweb.com>
2014-03-14 18:23:16 -0700

    Home Layout
    mobile and home layout almost complete.
    TODO: Footer, nav, slider, FB.

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="initial-scale=1">
-
-		<title>Semantic.gs + Sassaparilla</title>
-		<link rel="stylesheet" href="assets/css/add-ons/responsive-nav.min.css">
+		<title>ServiceMaster Restore</title>
 		<link href="assets/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>
 		<!-- <link rel="shortcut icon" type="image/ico" href="favicon.ico" /> -->
 		<!-- TODO: Download a custom build of modernizer to suit the project -->
@@ -17,30 +15,97 @@
 
 	<body>
 
-	<header>
-		<object class="logo" type="image/svg+xml" data="assets/img/logo.svg" alt="ServiceMaster Logo"></object>
 
-		<button href="#" type="button" role="button" class="nav-toggle closed lines-button x2"><span class="lines"></span></button>
-		
-		<nav class="nav-collapse">
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">About us</a></li>
-				<li><a href="">Services</a></li>
-				<li><a href="">Claims</a></li>
-				<li><a href="">FAQs</a></li>
-				<li><a href="">Contact us</a></li>
-			</ul>
-		</nav>
-		
+	<header>
+		<div class="wrapper">
+			<object class="logo" type="image/svg+xml" data="assets/img/logo.svg" alt="ServiceMaster Logo"></object>
+
+			<button href="#" type="button" role="button" class="nav-toggle closed lines-button x2"><span class="lines"></span></button>
+			
+			<nav class="nav-collapse">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About us</a></li>
+					<li><a href="">Services</a></li>
+					<li><a href="">Claims</a></li>
+					<li><a href="">FAQs</a></li>
+					<li><a href="">Contact us</a></li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 
+	<div class="wrapper">
+
+	<div id='slider' class='swipe'>
+	  <div class='swipe-wrap'>
+	    <div><img src="http://www.placehold.it/800x350" alt=""></div>
+	    <div><img src="http://www.placehold.it/800x350" alt=""></div>
+	    <div><img src="http://www.placehold.it/800x350" alt=""></div>
+	  </div>
+	</div>
+
+<!-- 	<div style='text-align:center;padding-top:20px;'>
+		<button onclick='mySwipe.prev()'>prev</button> 
+		<button onclick='mySwipe.next()'>next</button>
+	</div> -->
+
+	<div class="padded">
+		
+		<!-- TODO: Php this to above the NAV -->
+		<div class="callnow">
+			<h1>We're here for you 24/7</h1>
+			<a class="btn" href="tel+2086676633">Idaho Hotline: (208) 667-6633</a>
+			<a class="btn" href="tel+5099279416">Spokane Hotline: (509) 927-9416</a>
+		</div>
+
+
+		<section class="cta">
+			<figure>
+				<img src="http://www.placehold.it/400x175" alt="">
+				<h2>Current Projects</h2>
+			</figure>
+			<figure>
+				<img src="http://www.placehold.it/400x175" alt="">
+				<h2>Testamonials</h2>
+			</figure>
 			
+			<a href="#" class="tips">
+				<object type="image/svg+xml" data="assets/img/triangle.svg"></object>
+				<span>Help with <br> Prevention</span>
+			</a>
+			<a href="#" class="tips">
+				<object type="image/svg+xml" data="assets/img/triangle.svg"></object>
+				<span>Tips for <br> Mitigation</span>
+			</a>
+			<a href="#" class="tips">
+				<object type="image/svg+xml" data="assets/img/triangle.svg"></object>
+				<span>Privacy <br> Policy</span>
+			</a>
+			<a href="#" class="tips">
+				<object type="image/svg+xml" data="assets/img/triangle.svg"></object>
+				<span>Download <br> Forms</span>
+			</a>
+		</section>
+		<section class="fb">
+			<img src="http://www.placehold.it/500x300" alt="">
+		</section>
+
+	</div> <!-- END PADDED CONTENT -->
+
+	<div class="padded">
+		<footer>
+			<a href="#" class="btn">Employment</a><a href="" class="btn">Contact us</a>
+		</footer>
+	</div>
+
+	</div> <!-- END WRAPPER -->
 
 		<!-- TODO: Select version of jquery and link to CDN -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="assets/js/script.min.js"></script>
 		<script src="assets/js/responsive-nav.js"></script>
+		<script src="assets/js/swipe.min.js"></script>
 		<script>
 	      // var navigation = responsiveNav(".nav-collapse");
 
@@ -51,6 +116,23 @@
 			$(".nav-toggle").click(function(){
 				$(this).toggleClass("close");
 			});
+
+			// pure JS
+			var elem = document.getElementById('slider');
+			window.mySwipe = Swipe(elem, {
+			  // startSlide: 4,
+			  auto: 6000,
+			  // continuous: true,
+			  // disableScroll: true,
+			  // stopPropagation: true,
+			  // callback: function(index, element) {},
+			  // transitionEnd: function(index, element) {}
+			});
+
+			// with jQuery
+			// window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
+
+
 		</script>
 	</body>
 
