e80d28718b736f9570770f566090aaf33cb72808
Ryan Maskell <ryan@savorweb.com>
2014-03-18 15:48:18 -0700

    Facebook API success

--- index.php
+++ index.php
@@ -14,9 +14,16 @@
 	</head>
 
 	<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=763971193627319";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-
-	<header>
+	<header class="pagehead">
 		<div class="wrapper">
 			<object class="logo" type="image/svg+xml" data="assets/img/logo.svg" alt="SevrviceMaster Logo"></object>
 
@@ -88,8 +95,16 @@
 				<span>Download <br> Forms</span>
 			</a>
 		</section>
+
+				<!-- Pulling in Facebook page id:'524500907614152',
+		access_token:'763971193627319|IAUf3wjhHJoSZQLB2tRdI71AacA' -->
+		
 		<section class="fb">
-			
+			<header>
+				<h1>ServiceMaster of North Idaho</h1>
+				<div class="fb-like" data-href="https://www.facebook.com/pages/Servicemaster-of-North-Idaho/524500907614152" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+			</header>
+			<div id="wall" class="facebookWall"></div>
 		</section>
 
 	</div>
@@ -119,7 +134,8 @@
 
 		<!-- TODO: Select version of jquery and link to CDN -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="assets/js/script.min.js"></script>
+		<script src="assets/js/facebook.js"></script>
+		<script src="assets/js/jquery.tmpl.min.js"></script>
 		<script src="assets/js/responsive-nav.js"></script>
 		<script src="assets/js/swipe.min.js"></script>
 		<script>
@@ -150,7 +166,51 @@
 			// window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
 
 
-		</script>
+</script>
+<!-- jQuery templates. Not rendered by the browser. Notice the type attributes -->
+
+<script id="headingTpl" type="text/x-jquery-tmpl">
+<h1>${name}<span>on Facebook</span></h1>
+</script>
+
+<script id="feedTpl" type="text/x-jquery-tmpl">
+<li>
+	<img src="${from.picture}" class="avatar" />
+	
+	<div class="status">
+		<h2><a href="http://www.facebook.com/profile.php?id=${from.id}" target="_blank">${from.name}</a></h2>
+		<p class="message">{{html message}}</p>
+		{{if type == "link" }}
+			<div class="attachment">
+				{{if picture}}
+					<img class="picture" src="${picture}" />
+				{{/if}}
+				<div class="attachment-data">
+					<p class="name"><a href="${link}" target="_blank">${name}</a></p>
+					<p class="caption">${caption}</p>
+					<p class="description">${description}</p>
+				</div>
+			</div>
+		{{/if}}
+	</div>
+	
+	<p class="meta">${created_time} · 
+	{{if comments}}
+		${comments.count} Comment{{if comments.count>1}}s{{/if}}
+	{{else}}
+		0 Comments
+	{{/if}} · 
+	{{if likes}}
+		${likes.count} Like{{if likes.count>1}}s{{/if}}
+	{{else}}
+		0 Likes
+	{{/if}}
+	</p>
+	
+</li>
+</script>
+
+		<script src="assets/js/script.min.js"></script>
 	</body>
 
 </html>
