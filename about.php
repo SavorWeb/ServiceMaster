5c4151839b1b9f4e0dfa873e5643a421d0c19e41
Ryan Maskell <ryan@savorweb.com>
2014-03-18 17:31:09 -0700

    internal layout done

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php include("inc/head.html"); ?>
+
+	<div class="wrapper">
+	
+	<div class="pageimg">
+		<img src="assets/img/truckguy.jpg" alt="A Service Master!">
+	</div>
+
+	<div class="padded"> <!-- START CONTENT -->
+		
+		<section class="content">
+			<article>
+				<h1>Our Mission</h1>
+				<img src="http://www.placehold.it/500x500" alt="">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
+				<h2>Heading 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
+				<h3>Heading 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
+			</article>
+		</section>
+
+		<aside>
+			<h2>About Us</h2>
+			<ul>
+				<li><a href="">Link</a></li>
+				<li><a href="">Links</a></li>
+				<li><a href="">Really Long Link</a></li>
+				<li><a href="">Stuff</a></li>
+				<li><a href="">Test</a></li>
+			</ul>
+		</aside>
+		<aside>
+			<h2>Second Sidebar?</h2>
+			<ul>
+				<li><a href="">Link</a></li>
+				<li><a href="">Links</a></li>
+				<li><a href="">Really Long Link</a></li>
+				<li><a href="">Stuff</a></li>
+				<li><a href="">Test</a></li>
+			</ul>
+		</aside>
+
+
+	</div> <!-- END MAIN CONTENT -->
+
+	<?php include("inc/foot.html"); ?>
