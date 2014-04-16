dbc749e8f272890204b997600a568e8515aeaf5a
Ryan Maskell <ryan@savorweb.com>
2014-04-16 14:06:33 -0700

    claims content and social btns

--- claims.php
+++ claims.php
@@ -1,6 +1,6 @@
 <?php include("inc/head.html"); ?>
 
-	<div class="wrapper">
+<div class="wrapper">
 	
 	<div class="pageimg">
 		<img src="assets/photos/abovetrucks.jpg" alt="A Service Master!">
@@ -11,28 +11,108 @@
 		<section class="aside-layout">
 			<article class="editable">
 				<h1>Claims</h1>
-				<img src="http://www.placehold.it/500x500" alt="">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
-				<h2>Heading 2</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
-				<h3>Heading 3</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, in, quasi animi nulla quo necessitatibus corrupti harum dolores non labore! Eos, libero cumque quis doloribus quod beatae quas repellat sed.</p>
-			</article>
-		</section>
+				<img src="assets/photos/manwoman.jpg" alt="">
+				<p>ServiceMaster of North Idaho (SMNI) works with all insurance companies to restore losses back to pre-loss condition. We work directly with your insurance company, your claims adjusters, and we keep communication open with you for the entire process.  
+				</p><p>
+				While we are a preferred vendor for many insurance companies, we work with all insurance companies whether we are preferred vendor or not. The list of companies with whom we have worked is exhaustive so you can rest assured that you are in good hands with SMNI. Insurance companies and agents recommend SMNI because we consistently provide responsible mitigation and restoration services.
+			</p>
+			<h2>Terms</h2>
 
-		<aside>
-			<h2>Claim links</h2>
-			<ul>
-				<li><a href="">Link</a></li>
-				<li><a href="">Links</a></li>
-				<li><a href="">Really Long Link</a></li>
-				<li><a href="">Stuff</a></li>
-				<li><a href="">Test</a></li>
-			</ul>
-		</aside>
+			<p>In order to better help you understand some of the confusing terms that insurance companies sometimes use we have provided a list of terms and phrases with definitions below.</p>
+
+			<h3>RCV</h3>
+
+			<p>The dollar amount needed to replace damaged personal property or dwelling property without deducting for depreciation but may be limited by the maximum dollar allowed by your policy.
+			</p>
+			<h3>ACV</h3>
+
+			<p>Cost of replacing damaged or destroyed property with comparable new property, minus depreciation and obsolescence.
+			</p>
+			<h3>ACV Settlement</h3>
+
+			<p>The replacement cost value less the depreciation and the deuctible. Typcially this is the intial check you will receive from your insruance carrier.
+			</p>
+			<h3>Deprecation</h3>
+
+			<p>The decrease in value of an item due to wear, tear, or decay over time.
+			</p>
+			<h3>Recoverable Deprecation</h3>
+
+			<p>The amount held by the insurance carrier that is only able to be recovered or reimbursed to the homeowner if those expenses have actually been incurred. An itemized receipt, from the contractor, for all work completed is often required before any depreciation is released. The insurance company may also require a visual inspection of all completed work before realsing the depreciation check. 
+			</p>
+			<h3>Deductible</h3>
+
+			<p>The amount for which the insured (you) is liable on each loss.
+			</p>
+			<h3>Personal Property</h3>
+
+			<p>All tangible property that is either temporary or movable in some way, such as furniture, jewlery, electronics, etc…
+			</p>
+			<h3>Renters Insurance</h3>
+
+			<p>A form of insurance that covers a policyholder’s belongings against perils. It also provides personal liability coverage and additional living expenses. Possessions can be covered for their replacement cost or the actual cash value, which includes depreciation.
+			</p>
+
+		</article>
+	</section>
+	<aside>
+		<h2>State Farm Claims Center</h2>
+		<ul>
+			<li><a href="www.statefarm.com/claims">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 782-8332</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Farmers Help Point Claims Services</h2>
+		<ul>
+			<li><a href="www.farmers.com/claims">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 435-7764</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Allstate Claims</h2>
+		<ul>
+			<li><a href="www.allstate.com/claims.aspx">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 255-7828</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Metlife Superior Claims Service</h2>
+		<ul>
+			<li><a href="www.metlife.com/individual/insurance/home-insurance/superior-claims-service.html">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 854-6011</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Nationwide Claims Center</h2>
+		<ul>
+			<li><a href="www.nationwide.com/insurance-claims-center.jsp">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(877) 669-6877</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Travelers Claims Center</h2>
+		<ul>
+			<li><a href="www.travelers.com/claim/index.aspx">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 252-4633</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Safeco Claims Center</h2>
+		<ul>
+			<li><a href="www.safeco.com/claims-center">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 332-3226</a></li>
+		</ul>
+	</aside>
+	<aside>
+		<h2>Mutual of Enumclaw</h2>
+		<ul>
+			<li><a href="http://www.mutualofenumclaw.com/">Visit Website</a></li>
+			<li><a class="tel" href="tel:8007828332">(800) 366-5551</a></li>
+		</ul>
+	</aside>
 
 
-	</div> <!-- END MAIN CONTENT -->
+</div> <!-- END MAIN CONTENT -->
 
-	<?php include("inc/foot.html"); ?>
+<?php include("inc/foot.html"); ?>
