3e5509d5e2b263efa403e7d455808128ed2a4e8e
Ryan Maskell <ryan@savorweb.com>
2014-04-17 13:08:35 -0700

    fancybox plugin

--- current-projects.php
+++ current-projects.php
@@ -9,27 +9,82 @@
 	<div class="padded"> <!-- START CONTENT -->
 		
 		<section>
-			<article class="editable">
-<h1>Job 1594</h1>
+		<article>
+			<div class="editable full gallery">
+				<section class="repeatable">
+					<h1>Job 1594</h1>
 
-<p>Commerical production facility experience a fire sprinkler line break that flooded many areas of their facility.</p>
+					<p>Commerical production facility experience a fire sprinkler line break that flooded many areas of their facility.</p>
 
-<ul id="project-1" class="editable-gallery">
-    <li><a href="assets/photos/current-projects/job-1594/1.JPG"><img src="assets/photos/current-projects/job-1594/1.JPG" alt="Description"></a></li>
-</ul>
+					<ul id="project-1" class="editable-gallery">
+					    <li><a href="assets/photos/current-projects/job-1594/1.JPG"><img src="assets/photos/current-projects/job-1594/1.JPG" alt="Using our injectidry sytem to dry this hallway without removing any wall material."></a></li>
+					    <li><a href="assets/photos/current-projects/job-1594/2.JPG"><img src="assets/photos/current-projects/job-1594/2.JPG" alt="Flood cut of another hallway to allow for drying in the wall cavities and remove wet insulation"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1594/3.JPG"><img src="assets/photos/current-projects/job-1594/3.JPG" alt="Injectidry and dehu ducting into the wall cavity for more efficent drying"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1594/4.JPG"><img src="assets/photos/current-projects/job-1594/4.JPG" alt="More specialty drying techniques"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1594/5.JPG"><img src="assets/photos/current-projects/job-1594/5.JPG" alt="Injectidry system working on main hallway"></a></li>
+					</ul>
+				</section>
+				<section class="repeatable">
+					<h1>Job 1258</h1>
 
-<h1>Job 1258</h1>
+					<p>Customer experienced a large water loss due to a pipe break within the home. The entire home was affected including the upstairs living area and the basement shop.</p>
 
-<p>Customer experienced a large water loss due to a pipe break within the home. The entire home was affected including the upstairs living area and the basement shop.</p>
+					<ul id="project-2" class="editable-gallery">
+					    <li><a href="assets/photos/current-projects/job-1258/1.JPG"><img src="assets/photos/current-projects/job-1258/1.JPG" alt="Standing water effecting the carpet and pad in this living area"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/2.JPG"><img src="assets/photos/current-projects/job-1258/2.JPG" alt="Standing water extraction from carpeted living area"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/3.JPG"><img src="assets/photos/current-projects/job-1258/3.JPG" alt="Dehus set in the main living areas. Notice the use of corner guard to protect drywall from hoses. "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/4.JPG"><img src="assets/photos/current-projects/job-1258/4.JPG" alt="Furniture can be damaged when allowed to stay in contact with water saturated flooring. Here we  have use blocks to keep the furniture off the affected carpet and pad. "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/5.JPG"><img src="assets/photos/current-projects/job-1258/5.JPG" alt="Living area after affected, non-salvagable carpet is removed and dehus are set to begin drying"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/6.JPG"><img src="assets/photos/current-projects/job-1258/6.JPG" alt="Water damaged drywall falling on contents"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/7.JPG"><img src="assets/photos/current-projects/job-1258/7.JPG" alt="More water logged drywall"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/8.JPG"><img src="assets/photos/current-projects/job-1258/8.JPG" alt="Standing water in basement workshop bellow living area "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/9.JPG"><img src="assets/photos/current-projects/job-1258/9.JPG" alt="Extracting standing water from concrete"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/10.JPG"><img src="assets/photos/current-projects/job-1258/10.JPG" alt="Equipment set for drying"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1258/11.JPG"><img src="assets/photos/current-projects/job-1258/11.JPG" alt="Shop contents packed out and staged for transportation to our facilities"></a></li>
+					</ul>
+				</section>
+				<section class="repeatable">
+					<h1>Job 1168</h1>
 
-<h1>Job 1168</h1>
+					<p>Commercial and residental building that experienced a water loss in a top residential unit that affected several floors below which included some commerical spaces.</p>
 
-<p>Commercial and residental building that experienced a water loss in a top residential unit that affected several floors below which included some commerical spaces. </p>
+					<ul id="project-3" class="editable-gallery">
+					    <li><a href="assets/photos/current-projects/job-1168/1.JPG"><img src="assets/photos/current-projects/job-1168/1.JPG" alt="Affected ceiling tiles "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/2.JPG"><img src="assets/photos/current-projects/job-1168/2.JPG" alt="Removing the affected tiles "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/3.JPG"><img src="assets/photos/current-projects/job-1168/3.JPG" alt="Cleaning the debris left from the removal of the affected tiles"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/4.JPG"><img src="assets/photos/current-projects/job-1168/4.JPG" alt="Drying of ceiling cavity using ducting and injecti dry"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/5.JPG"><img src="assets/photos/current-projects/job-1168/5.JPG" alt="Dehus set to help control moisture in the environment"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/6.JPG"><img src="assets/photos/current-projects/job-1168/6.JPG" alt="Running dehu drip lines through a trafficed hallway, notice the use of tape to keep lines from tripping people up"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/7.JPG"><img src="assets/photos/current-projects/job-1168/7.JPG" alt="Moving of file cabinets to access affected drywall behind them"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/8.JPG"><img src="assets/photos/current-projects/job-1168/8.JPG" alt="Using the injeti dry system to dry out wall without having to remove drywall "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/9.JPG"><img src="assets/photos/current-projects/job-1168/9.JPG" alt="Water saturated carpet pad in the residential unit"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/10.JPG"><img src="assets/photos/current-projects/job-1168/10.JPG" alt="Removal of affected pad"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/11.JPG"><img src="assets/photos/current-projects/job-1168/11.JPG" alt="Falling drywall affected granite counter tops – BEFORE"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/12.JPG"><img src="assets/photos/current-projects/job-1168/12.JPG" alt="After our cleaning crew got done cleaning those counter tops– AFTER"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/13.JPG"><img src="assets/photos/current-projects/job-1168/13.JPG" alt="Equipment set for drying, we place a barrier between our equipment and the counter tops to prevent damage "></a></li>
+					    <li><a href="assets/photos/current-projects/job-1168/14.JPG"><img src="assets/photos/current-projects/job-1168/14.JPG" alt="After we removed the microwave and built in cabinetry for drying purposes,  they were laid out in the living room on plastic (to protect the carpet from and inadvertant staining) to await reinstallation "></a></li>
+					</ul>
+				</section>
+				<section class="repeatable">
+					<h1>Job 1378</h1>
 
-<h1>Job 1378</h1>
+					<p>A home fire result that affected this whole home resulting in the need to completely gut and rebuild.</p>
 
-<p>A home fire result that affected this whole home resulting in the need to completely gut and rebuild.</p>
+					<ul id="project-4" class="editable-gallery">
+					    <li><a href="assets/photos/current-projects/job-1378/1.JPG"><img src="assets/photos/current-projects/job-1378/1.JPG" alt="For our initial response we boarded up a hole in the roof made for ventalation by the Fire Department"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/2.JPG"><img src="assets/photos/current-projects/job-1378/2.JPG" alt="Weather proofing the board up area"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/3.JPG"><img src="assets/photos/current-projects/job-1378/3.JPG" alt="A serverly affected kitchen area, those cabinets are covered in soot not paint"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/4.JPG"><img src="assets/photos/current-projects/job-1378/4.JPG" alt="A better look at that affected kitchen"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/5.JPG"><img src="assets/photos/current-projects/job-1378/5.JPG" alt="That same kitchen now"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/6.JPG"><img src="assets/photos/current-projects/job-1378/6.JPG" alt="The basment affected by fire and water damage. The water damage results when the fire department puts out the fire creating an even disaster"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/7.JPG"><img src="assets/photos/current-projects/job-1378/7.JPG" alt="The basement after all affected items have been removed"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/8.JPG"><img src="assets/photos/current-projects/job-1378/8.JPG" alt="That basement now"></a></li>
+					    <li><a href="assets/photos/current-projects/job-1378/9.JPG"><img src="assets/photos/current-projects/job-1378/9.JPG" alt="Putting some finishing touches on the home before our customer move back in"></a></li>
+					</ul>
+				</section>
 
+
+			</div>
 			</article>
 		</section>
 
