09e1921d01aaa0a17a3feb010fb437d18c863c29
Ryan Maskell <ryan@savorweb.com>
2014-04-17 11:31:15 -0700

    current projects

--- current-projects.php
+++ current-projects.php
@@ -10,8 +10,25 @@
 		
 		<section>
 			<article class="editable">
-				<h1>Current Projects</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ducimus quae assumenda facilis doloribus. Odio, magnam, blanditiis, assumenda a in voluptates eum neque ipsam ipsum eligendi tenetur aperiam labore fugit!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ducimus quae assumenda facilis doloribus. Odio, magnam, blanditiis, assumenda a in voluptates eum neque ipsam ipsum eligendi tenetur aperiam labore fugit!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ducimus quae assumenda facilis doloribus. Odio, magnam, blanditiis, assumenda a in voluptates eum neque ipsam ipsum eligendi tenetur aperiam labore fugit!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ducimus quae assumenda facilis doloribus. Odio, magnam, blanditiis, assumenda a in voluptates eum neque ipsam ipsum eligendi tenetur aperiam labore fugit!</p>
+<h1>Job 1594</h1>
+
+<p>Commerical production facility experience a fire sprinkler line break that flooded many areas of their facility.</p>
+
+<ul id="project-1" class="editable-gallery">
+    <li><a href="assets/photos/current-projects/job-1594/1.JPG"><img src="assets/photos/current-projects/job-1594/1.JPG" alt="Description"></a></li>
+</ul>
+
+<h1>Job 1258</h1>
+
+<p>Customer experienced a large water loss due to a pipe break within the home. The entire home was affected including the upstairs living area and the basement shop.</p>
+
+<h1>Job 1168</h1>
+
+<p>Commercial and residental building that experienced a water loss in a top residential unit that affected several floors below which included some commerical spaces. </p>
+
+<h1>Job 1378</h1>
+
+<p>A home fire result that affected this whole home resulting in the need to completely gut and rebuild.</p>
 
 			</article>
 		</section>
